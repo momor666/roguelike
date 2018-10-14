08f5e11cc65674221ae37f0614d7b1adf3ee86e3
Rafael Hart <rafael.hart9@gmail.com>
2018-10-14 21:58:37 +1100

    Create the player and an NPC as objects

--- engine.py
+++ engine.py
@@ -18,6 +18,28 @@ tcod.sys_set_fps(LIMIT_FPS)
 player_x = SCREEN_WIDTH // 2
 player_y = SCREEN_HEIGHT // 2
 
+class Object:
+    """ This is a generic object, eg the player, a monster, an item """
+    def __init__(self, x, y, char, color):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move(self, dx, dy):
+        # Move by the given distance
+        self.x += dx
+        self.y += dy
+
+    def draw(self):
+        # Set the color and then draw the char that represents the object at that position
+        tcod.console_set_default_foreground(con, self.color)
+        tcod.console_put_char(con, self.x, self.y, ' ', tcod.BKGND_NONE)
+
+    def clear(self):
+        # Erase the character that represents this object
+        tcod.console_put_char(con, self.x, self.y, ' ', tcod.BKGND_NONE)
+
 def handle_keys():
     global player_x, player_y
 
@@ -43,12 +65,28 @@ def handle_keys():
     elif tcod.console_is_key_pressed(tcod.KEY_RIGHT):
         player_x = player_x + 1
 
+# Create the player
+player = Object(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2, '@', tcod.white)
+
+# Create an NPC
+npc = Object(SCREEN_WIDTH // 2 - 5, SCREEN_HEIGHT // 2, '@', tcod.yellow)
+
+# List of objects
+objects = [npc, player]
 
 while not tcod.console_is_window_closed():
-    tcod.console_set_default_foreground(0, tcod.white)
-    tcod.console_put_char(con, player_x, player_y, '@', tcod.BKGND_NONE)
+
+    # Draw all objects in the list
+    for object in objects:
+        object.draw()
+
+    # Blit the contents of 'con' to the root console and present it
     tcod.console_blit(con, 0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, 0, 0, 0) # Push to the root console
     tcod.console_flush() # Present changes to the screen
+
+    # Erase all objects at their old locations, before they move
+    for object in objects:
+        object.clear()
 
     # Handle keys and exit game if required:
     tcod.console_put_char(con, player_x, player_y, ' ', tcod.BKGND_NONE)
