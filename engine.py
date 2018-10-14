2d4e57ab99b925dec4aab10b6667976352475e6c
Rafael Hart <rafael.hart9@gmail.com>
2018-10-14 21:15:40 +1100

    Setting up libtcod

--- engine.py
+++ engine.py
@@ -1,8 +1,20 @@
-import libtcodpy as libtcod
+import libtcodpy as tcod
 
-def main():
-    print('Hello World!')
+SCREEN_WIDTH = 80
+SCREEN_HEIGHT = 50
+LIMIT_FPS = 20
 
-if __name__ == '__main__':
-    main()
+font_path = 'arial12x12.png'
+font_flags = tcod.FONT_TYPE_GREYSCALE | tcod.FONT_LAYOUT_TCOD
+tcod.console_set_custom_font(font_path, font_flags)
 
+# Initialize the Window
+window_title = 'Roguelike'
+fullscreen = False
+tcod.console_init_root(SCREEN_WIDTH, SCREEN_HEIGHT, window_title, fullscreen)
+
+tcod.sys_set_fps(LIMIT_FPS)
+while not tcod.console_is_window_closed():
+    tcod.console_set_default_foreground(0, tcod.white)
+    tcod.console_put_char(0, 1, 1, '@', tcod.BKGND_NONE)
+    tcod.console_flush() # present the changes to the screen
